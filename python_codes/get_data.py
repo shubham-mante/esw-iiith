d645a3cec95a343b4e50be6dfc9c5f483f36522c
shubham-mante <shubham.mante@research.iiit.ac.in>
2021-10-12 16:13:50 +0530

    "added new python codes"

--- python_codes/get_data.py
+++ python_codes/get_data.py
@@ -2,34 +2,30 @@ from oneM2M_functions import *
 
 server = "http://127.0.0.1:8080"
 cse = "/~/in-cse/in-name/"
-
-
-# ------------------------------------------
-# Fill code here to create AE
-# specified by the URI
-# ------------------------------------------
 ae = "Test-1"
-lbl_ae = ["AE-Label-1", "AE-Label-2"]
-create_ae(server+cse, ae, lbl_ae)
-# ------------------------------------------
-
-
-# ------------------------------------------
-# Fill code here to create container in the AE
-# specified by the URI
-# ------------------------------------------
 container_name = "Data"
-lbl_cnt = ["CNT-Label-1", "CNT-Label-2"]
-create_cnt(server+cse+ae, container_name, lbl_cnt)
-# ------------------------------------------
-
 
 # ------------------------------------------
-# Fill code here to create content_instance
+# Fill code here to get latest content_instance
 # specified by the URI
 # ------------------------------------------
-content_instance = 0
-lbl_cin = ["CIN-Label-1", "CIN-Label-2"]
-create_data_cin(server+cse+ae+"/"+container_name, content_instance, lbl_cin)
+ret_code, latest_data = get_data(server+cse+ae+"/Data/la")
+print(latest_data)
 # ------------------------------------------
 
+# ------------------------------------------
+# Fill code here to get oldest content_instance
+# specified by the URI
+# ------------------------------------------
+# ret_code, oldest_data = get_data(server+cse+ae+"/Data/ol")
+# print(oldest_data)
+# ------------------------------------------
+
+# ------------------------------------------
+# Fill code here to get all content_instances
+# specified by the URI
+# Note: change the return statement as given in get_data function inside the oneM2M_functions.py
+# ------------------------------------------
+# ret_code, all_data = get_data(server+cse+ae+"/Data?rcn=4")
+# print(all_data)
+# ------------------------------------------
